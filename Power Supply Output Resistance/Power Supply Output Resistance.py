28d30a6c630d9c3bc0b036fbb3607c865b3ca0a8
Martagonczar <martagonczaris@gmail.com>
2022-10-18 11:04:39 -0400

    Update Power Supply Output Resistance.py
    code

--- Power Supply Output Resistance/Power Supply Output Resistance.py
+++ Power Supply Output Resistance/Power Supply Output Resistance.py
@@ -1,5 +1,12 @@
 import numpy as np
+import matplotlib.pyplot as plt
+OCV1, uOCV1, R1, uR1, TV1, uTV1, I1, uI1, OCV2, uOCV2, R2, uR2, TV2, uTV2, I2, uI2 = np.loadtxt("PSdata2.csv", delimiter=",", unpack=True)
+#part 1
+#(a)Plot V vs I and use the data to determine the output resistance of the battery Rb.
+plt.plot(TV1,I1, marker="o",linestyle="None")
+plt.xlabel("Battery Terminal Voltage (V)")
+plt.ylabel("Current (I)")
+plt.title("The Position Of A Rocket over Time")
+#(b) Estimate all uncertainties.
 
-V1, uV1, I1, uI1, R1, uR1 = np.loadtxt("PSdata2.csv", delimiter=",", unpack=True)
-
-#qwertyui
+plt.errorbar(time, position, yerr=up,fmt="o",ecolor="blue")
