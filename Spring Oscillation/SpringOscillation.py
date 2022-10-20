0d6feb6fd48c6b33929de86e6dd24c01c67711c5
Nick Garcia <nicholasgarcia2003@protonmail.com>
2022-10-20 10:20:57 -0400

    Coding for the Experiment section of Oscillating Spring

--- Spring Oscillation/SpringOscillation.py
+++ Spring Oscillation/SpringOscillation.py
@@ -1,2 +1,26 @@
 #NICHOLAS GARCIA & MARTA GONCZAR
 """OSCILLATING MASS ON A SPRING"""
+
+import numpy as np
+import scipy as sp
+import matplotlib.pyplot as pl
+
+m = 200.7; #g
+T = 5.11 / 7; #seconds / oscillations = period
+
+xt, x, ux = np.loadtxt("x.txt", unpack=True)
+
+#converting from cm to m
+x = x / 100 
+ux = ux / 100
+
+pl.cla()
+pl.plot(xt,x)
+pl.xlabel("Time (s)")
+pl.ylabel("Distance (m)")
+pl.savefig("Distance over Time")
+
+total_time = xt[-1] - xt[0]
+number_of_oscillations_in_plot = 7
+T_fromPlot = total_time / number_of_oscillations_in_plot
+print("\nFrom the plotted data, the period of the oscillation is ", T_fromPlot," seconds.")
