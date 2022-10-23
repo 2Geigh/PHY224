5719cc21910a6006134360ef4119b3a8ec9c7c3f
Martagonczar <martagonczaris@gmail.com>
2022-10-23 15:04:10 -0400

    updated

--- Power Supply Output Resistance/Power Supply Output Resistance.py
+++ Power Supply Output Resistance/Power Supply Output Resistance.py
@@ -3,13 +3,14 @@ import matplotlib.pyplot as plt
 Battery_OpenCircuitVoltage, uBattery_OpenCircuitVoltage, Battery_Resistance, uBattery_Resistance, Battery_TerminalVoltage, uBattery_TerminalVoltage, Battery_Current, uBattery_Current = np.loadtxt("BATTERYdata.csv", delimiter=",", unpack=True)
 DCSupply_OpenCircuitVoltage, uDCSupply_OpenCircuitVoltage, DCSupply_Resistance, uDCSupply_Resistance, DCSupply_TerminalVoltage, uDCSupply_TerminalVoltage, DCSupply_Current, uDCSupply_Current = np.loadtxt("DCSUPPLYdata.csv", delimiter=",", unpack=True)
 #6.5V
-65V_OpenCircuitVoltage, u65V_OpenCircuitVoltage, 65V_Resistance, u65V_Resistance, 65V_TerminalVoltage, u65V_TerminalVoltage, 65V_Current, u65V_Current = np.loadtxt("6.5V.csv", delimiter=",", unpack=True)
+V65_OpenCircuitVoltage, u65V_OpenCircuitVoltage, V65_Resistance, u65V_Resistance, V65_TerminalVoltage, u65V_TerminalVoltage, V65_Current, u65V_Current = np.loadtxt("6.5V.csv", delimiter=",", unpack=True)
 #10v
-10V_OpenCircuitVoltage, u10V_OpenCircuitVoltage, 10V_Resistance, u10V_Resistance, 10V_TerminalVoltage, u10V_TerminalVoltage, 10V_Current, u10V_Current = np.loadtxt("10V.csv", delimiter=",", unpack=True)
+V10_OpenCircuitVoltage, u10V_OpenCircuitVoltage, V10_Resistance, u10V_Resistance, V10_TerminalVoltage, u10V_TerminalVoltage, V10_Current, u10V_Current = np.loadtxt("10V.csv", delimiter=",", unpack=True)
 #15V
-15V_OpenCircuitVoltage, u15V_OpenCircuitVoltage, 15V_Resistance, u15V_Resistance, 15V_TerminalVoltage, u15V_TerminalVoltage, 15V_Current, u15V_Current = np.loadtxt("15V.csv", delimiter=",", unpack=True)
+V15_OpenCircuitVoltage, u15V_OpenCircuitVoltage, V15_Resistance, u15V_Resistance, V15_TerminalVoltage, u15V_TerminalVoltage, V15_Current, u15V_Current = np.loadtxt("15V.csv", delimiter=",", unpack=True)
 #20v
-20V_OpenCircuitVoltage, u20V_OpenCircuitVoltage, 20V_Resistance, u20V_Resistance, 20V_TerminalVoltage, u20V_TerminalVoltage, 20V_Current, u20V_Current = np.loadtxt("20V.csv", delimiter=",", unpack=True)
+V20_OpenCircuitVoltage, u20V_OpenCircuitVoltage, V20_Resistance, u20V_Resistance, V20_TerminalVoltage, u20V_TerminalVoltage, V20_Current, u20V_Current = np.loadtxt("20V.csv", delimiter=",", unpack=True)
+
 
 #part 1
 #(a)Plot V vs I and use the data to determine the output resistance of the battery Rb.
@@ -32,11 +33,31 @@ plt.title("Battery Terminal voltage vs. Current")
 plt.errorbar(DCSupply_Current,DCSupply_TerminalVoltage, xerr=uDCSupply_Current, yerr=uDCSupply_TerminalVoltage,fmt="o",ecolor="blue")
 plt.show()
 #plot 6.5
-
-plt.plot(65V_Current,65V_TerminalVoltage, marker="o",linestyle="None")
+plt.plot(V65_Current,V65_TerminalVoltage, marker="o",linestyle="-")
 plt.ylabel("Battery Terminal Voltage (V)")
 plt.xlabel("Current (I)")
-plt.title("Battery Terminal voltage vs. Current")
-#(c) Estimate all uncertainties.
-plt.errorbar(65V_Current,65V_TerminalVoltage, xerr=u65V_Current, yerr=u65V_TerminalVoltage,fmt="o",ecolor="blue")
+plt.title("Battery Terminal voltage 6.5V vs. Current")
+plt.errorbar(V65_Current,V65_TerminalVoltage, xerr=u65V_Current, yerr=u65V_TerminalVoltage,fmt="o",ecolor="blue")
 plt.show()
+#plot 10
+plt.plot(V10_Current,V10_TerminalVoltage, marker="o",linestyle="-")
+plt.ylabel("Battery Terminal Voltage (V)")
+plt.xlabel("Current (I)")
+plt.title("Battery Terminal voltage 10V vs. Current")
+plt.errorbar(V10_Current,V10_TerminalVoltage, xerr=u10V_Current, yerr=u10V_TerminalVoltage,fmt="o",ecolor="blue")
+plt.show()
+# PLOT 15
+plt.plot(V15_Current,V15_TerminalVoltage, marker="o",linestyle="-")
+plt.ylabel("Battery Terminal Voltage (V)")
+plt.xlabel("Current (I)")
+plt.title("Battery Terminal voltage 15V vs. Current")
+plt.errorbar(V15_Current,V15_TerminalVoltage, xerr=u15V_Current, yerr=u15V_TerminalVoltage,fmt="o",ecolor="blue")
+plt.show()
+#PLOT 20
+plt.plot(V20_Current,V20_TerminalVoltage, marker="o",linestyle="-")
+plt.ylabel("Battery Terminal Voltage (V)")
+plt.xlabel("Current (I)")
+plt.title("Battery Terminal voltage 20V vs. Current")
+plt.errorbar(V20_Current,V20_TerminalVoltage, xerr=u20V_Current, yerr=u20V_TerminalVoltage,fmt="o",ecolor="blue")
+plt.show()
+
