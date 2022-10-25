f40901b68f1973db2f6316aee8a1cf98e889d775
Martagonczar <martagonczaris@gmail.com>
2022-10-25 00:20:03 -0400

    edited

--- Power Supply Output Resistance/Power Supply Output Resistance.py
+++ Power Supply Output Resistance/Power Supply Output Resistance.py
@@ -16,7 +16,7 @@ V20_OpenCircuitVoltage, u20V_OpenCircuitVoltage, V20_Resistance, u20V_Resistance
 #(a)Plot V vs I and use the data to determine the output resistance of the battery Rb.
 plt.plot(Battery_Current,Battery_TerminalVoltage, marker="o",linestyle="-")
 plt.ylabel("Battery Terminal Voltage (V)")
-plt.xlabel("Current (I)")
+plt.xlabel("Current (A)")
 plt.title("Battery Terminal voltage vs. Current")
 #(b) Estimate all uncertainties.
 plt.errorbar(Battery_Current,Battery_TerminalVoltage, xerr=uBattery_Current,yerr=uBattery_TerminalVoltage,fmt="o",ecolor="blue")
@@ -27,7 +27,7 @@ plt.show()
 
 plt.plot(DCSupply_Current,DCSupply_TerminalVoltage, marker="o",linestyle="None")
 plt.ylabel("DC Power SUpply Voltage (V)")
-plt.xlabel("Current (I)")
+plt.xlabel("Current (mA)")
 plt.title("DC Power SUpply Voltage vs. Current")
 #(c) Estimate all uncertainties.
 plt.errorbar(DCSupply_Current,DCSupply_TerminalVoltage, xerr=uDCSupply_Current, yerr=uDCSupply_TerminalVoltage,fmt="o",ecolor="blue")
@@ -35,29 +35,61 @@ plt.show()
 #plot 6.5
 plt.plot(V65_Current,V65_TerminalVoltage, marker="o",linestyle="-")
 plt.ylabel("DC Power SUpply Voltage (V)")
-plt.xlabel("Current (I)")
+plt.xlabel("Current (mA)")
 plt.title("DC Power SUpply Voltage 6.5V vs. Current")
 plt.errorbar(V65_Current,V65_TerminalVoltage, xerr=u65V_Current, yerr=u65V_TerminalVoltage,fmt="o",ecolor="blue")
 plt.show()
 #plot 10
 plt.plot(V10_Current,V10_TerminalVoltage, marker="o",linestyle="-")
 plt.ylabel("DC Power SUpply Voltage (V)")
-plt.xlabel("Current (I)")
+plt.xlabel("Current (mA)")
 plt.title("DC Power SUpply Voltage 10V vs. Current")
 plt.errorbar(V10_Current,V10_TerminalVoltage, xerr=u10V_Current, yerr=u10V_TerminalVoltage,fmt="o",ecolor="blue")
 plt.show()
 # PLOT 15
 plt.plot(V15_Current,V15_TerminalVoltage, marker="o",linestyle="-")
 plt.ylabel("DC Power SUpply Voltage (V)")
-plt.xlabel("Current (I)")
+plt.xlabel("Current (mA)")
 plt.title("DC Power SUpply Voltage 15V vs. Current")
 plt.errorbar(V15_Current,V15_TerminalVoltage, xerr=u15V_Current, yerr=u15V_TerminalVoltage,fmt="o",ecolor="blue")
 plt.show()
 #PLOT 20
 plt.plot(V20_Current,V20_TerminalVoltage, marker="o",linestyle="-")
 plt.ylabel("DC Power SUpply Voltage (V)")
-plt.xlabel("Current (I)")
+plt.xlabel("Current (mA)")
 plt.title("DC Power SUpply Voltage 20V vs. Current")
 plt.errorbar(V20_Current,V20_TerminalVoltage, xerr=u20V_Current, yerr=u20V_TerminalVoltage,fmt="o",ecolor="blue")
 plt.show()
 
+output_resitance=[0.676, 1.092, 1.704,7.619]
+uoutput_resitance=[0.180, 0.395, 0.900, 8.594]
+plt.plot(Battery_Resistance, output_resitance,marker="o",linestyle="-")
+plt.xlabel("Battery Resistance (ohms)")
+plt.ylabel("Output resitance (ohms)")
+plt.title("Battery Resitance Vs Output Resistance")
+plt.errorbar(Battery_Resistance, output_resitance, xerr=uBattery_Resistance, yerr=uoutput_resitance,fmt="o",ecolor="blue")
+plt.show()
+
+output_res2=[0.0955261901, 1.835168502, 1.794430089,1.661129568,1.83286735,1.822521154, 1.819623944, 1.888319396,1.844660194,1.8287373,1.838006231,1.798237727,1.848563969,1.832061069,1.799065421, 1.618122977]
+uoutput_res2=[0.1674060868,
+0.3561981456,
+0.7628640184,
+4.397863986,
+0.1486708436,
+0.3084644404,
+0.6601726868,
+3.804940914,
+0.1351749862,
+0.2798466889,
+0.5975514116,
+3.435697156,
+0.1285870924,
+0.2654809493,
+0.5655713381,
+3.249717658]
+plt.plot(DCSupply_Resistance, output_res2,marker="o",linestyle="none")
+plt.xlabel("Battery Resistance (ohms)")
+plt.ylabel("Output resitance (ohms)")
+plt.title("Battery Resitance Vs Output Resistance")
+plt.errorbar(DCSupply_Resistance, output_res2, xerr=uDCSupply_Resistance, yerr=uoutput_res2,fmt="o",ecolor="blue")
+plt.show()
