5f777fb466ecd5492c6e05f7d44019c17dfda62c
Martagonczar <martagonczaris@gmail.com>
2022-11-17 11:44:05 -0500

    added activity 2 plot

--- wave_phenomena-ex2.py
+++ wave_phenomena-ex2.py
@@ -9,7 +9,9 @@ frequency= [5,10,12,15,20]
 avg_wavelength=[4.117,1.9,1.59,1.27,0.972]
 uavg_wavelength=[0.4117,0.19,0.2385,0.1905,0.1458]
 import  matplotlib.pyplot as plt
+plt.cla()
 plt.plot(frequency,avg_wavelength,".")
 plt.errorbar(frequency,avg_wavelength, yerr=uavg_wavelength, fmt="none");
 plt.xlabel("Frequency (Hz)");
 plt.ylabel("Average Wavelength (cm)")
+plt.savefig("ACTIVITY 2")
